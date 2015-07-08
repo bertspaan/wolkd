ec591ba732c4d75c7ed17a57dec63ae2195a45e0
Jelle Herold <jelle@defekt.nl>
2015-07-09 00:18:40 +0200

    andere settings

--- animations/functions.js
+++ animations/functions.js
@@ -66,11 +66,11 @@ module.exports = {
 		var dx = x - 0.5;
 		var dy = y - 0.5;
 		
-		var k = Math.abs(Math.sin((t/24)))
+		var k = Math.abs(Math.sin((t/16)))
 		var a = Math.atan2(dy, dx) / Math.PI;
-		var rad = (a + (t/16)) % 1;
+		var rad = (a + (t/32)) % 1;
 		// var v = (rad < .15) ? 255 : 0;
-		var v = Math.pow(rad, 3) * (k > 0.7 ? 255 : 0);
+		var v = Math.pow(rad, 3) * (k > 0.8 ? 255 : 0);
 
 		return [v, v, v]
 	},
