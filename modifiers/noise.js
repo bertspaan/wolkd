25c5534fb446d55eed78fc6a4ba34f07349cc794
Bert Spaan <bert.spaan@gmail.com>
2015-07-15 01:53:28 +0200

    better noise!

--- modifiers/noise.js
+++ modifiers/noise.js
@@ -1,11 +1,17 @@
+var Color = require('color');
+
 var max = 255;
 
 module.exports =  {
   title: 'Noise',
   getPixel: function(rgb, value) {
-    return rgb.map(function(c) {
-      var rand = Math.round(Math.random() * (max * value) * 2);
-      return Math.max(Math.min(c - (max * value) + rand, 255), 0);
-    });
+    var color = Color().rgb(rgb);
+    var rand = Math.random() * value * 2 - 1;
+    if (rand > 0) {
+      color.lighten(rand);
+    } else {
+      color.darken(rand);
+    }
+    return color.rgbArray();
   }
 };
